26dfcb3f0b1fe6f203e4831fadaf1ae5974c5922
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-23 06:03:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
+
+
+
+
 ReactDOM.render(
   <div>Components Need To Be Mounted</div>,
   document.getElementById('root')
