fed71dce0b893cd19c02413a83ba340d94ec94d6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-23 06:04:51 +0000

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,24 @@
 // Component Code Goes Here
+
+import React from 'react';
+
+class Address extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullAddress: `${props.street}, ${props.city}`
+    }
+  } 
+
+  render() {
+    return (
+      <div className="address">
+        {this.state.street},
+        {this.props.city}
+      </div>
+    );
+  }
+}
+
+export default Address; 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
+
+
+
+
 ReactDOM.render(
+  
+    <ToggleButton />,
+  <Address street="Santa Monica Blvd." city="Santa Monica" />,
+  
   <div>Components Need To Be Mounted</div>,
   document.getElementById('root')
 )
